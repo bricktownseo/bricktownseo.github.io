e054f60bd95272582967f76d206d8064d73f15d4
summitghimire <summitghimire@Summits-MacBook-Air.local>
2015-07-22 16:37:42 -0500

    try

--- assets/js/app.js
+++ assets/js/app.js
@@ -181,7 +181,7 @@ angular.module('SEMRushApp')
 
 
         $scope.semkey = $window.localStorage['semkey'];
-        $scope.store = $window.localStorage['checkedOrNot']
+        $scope.store = $window.localStorage['checkedOrNot'];
         $scope.reset();
 
     }]);
