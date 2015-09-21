fb7da9b04b30b013cbc987c1920fbdb025ddf17e
Phil Switzer <bricktownseo@gmail.com>
2015-09-21 11:24:46 -0500

    Form Updates

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -19,6 +19,15 @@ angular.module('Contact').controller('ContactCtrl', ['$scope', function($scope) 
 	
 	Parse.initialize("hLyeV7y6Uo3Ga3fSZgOu87j5bmJahVS3MGqcaZxg", "6MoiN7pUZ4FnZwXDXXTYiy2ft7aXdMSIS1J5qd3k");
 
-	$scope.data = 'TEST';
+	$scope.contact = function(){
+		console.log($scope);
+		var ContactObject = Parse.Object.extend("Contact");
+		var contactObject = new ContactObject();
+		contactObject.save({foo: "bar"}).then(function(object) {
+		  alert("yay! it worked");
+		});
+
+		$scope.data = 'TEST';
+	};
 
 }]);
