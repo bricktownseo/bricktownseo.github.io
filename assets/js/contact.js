b3c3f4f0743bd3f6dc290024e56b050c372d4c5b
Phil Switzer <bricktownseo@gmail.com>
2015-09-21 13:46:17 -0500

    added thanks.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,14 +9,14 @@ angular
 
 angular.module('Contact').controller('ContactCtrl', ['$scope', function($scope) {
 
-	console.log('CONTACT CTRL');
-
 	$scope.contact = function(){
-		
-		console.log($scope);
-		
+
 		if($scope.name!=null && $scope.name.length==0){
 			alert("Please enter your name.");
+			return;
+		}
+		if($scope.phone!=null && $scope.phone.length==0){
+			alert("Please enter your phone.");
 			return;
 		}
 		if($scope.email!=null && $scope.email.length==0){
@@ -31,11 +31,12 @@ angular.module('Contact').controller('ContactCtrl', ['$scope', function($scope) 
 		var ContactObject = Parse.Object.extend("Contact");
 		var contactObject = new ContactObject();
 		contactObject.set("Name", $scope.name);
+		contactObject.set("Phone", $scope.phone);
 		contactObject.set("Email", $scope.email);
 		contactObject.set("Message", $scope.message);
 
 		contactObject.save().then(function(object) {
-		  alert("yay! it worked");
+			$scope.thanks = true;
 		});
 
 		$scope.data = 'TEST';
