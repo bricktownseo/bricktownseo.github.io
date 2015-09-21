d7d1698099107e6096d8e6e78528c646e8aaf662
Phil Switzer <bricktownseo@gmail.com>
2015-09-21 13:42:04 -0500

    saving contact.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -12,17 +12,33 @@ angular.module('Contact').controller('ContactCtrl', ['$scope', function($scope) 
 	console.log('CONTACT CTRL');
 
 	$scope.contact = function(){
-		alert('CONTACT');
-		/*
+		
 		console.log($scope);
+		
+		if($scope.name!=null && $scope.name.length==0){
+			alert("Please enter your name.");
+			return;
+		}
+		if($scope.email!=null && $scope.email.length==0){
+			alert("Please enter your message.");
+			return;
+		}
+		if($scope.message!=null && $scope.message.length==0){
+			alert("Please enter your message.");
+			return;
+		}
+		
 		var ContactObject = Parse.Object.extend("Contact");
 		var contactObject = new ContactObject();
-		contactObject.save({foo: "bar"}).then(function(object) {
+		contactObject.set("Name", $scope.name);
+		contactObject.set("Email", $scope.email);
+		contactObject.set("Message", $scope.message);
+
+		contactObject.save().then(function(object) {
 		  alert("yay! it worked");
 		});
 
 		$scope.data = 'TEST';
-		*/
 	};
 
 }]);
